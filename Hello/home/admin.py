e641407a5dfa6add4851bba091321064e8ac5757
Vanshika Thapliyal <vanshika.thapliyal2002@gmail.com>
2022-07-05 14:20:05 +0530

    Delete Hello directory

--- Hello/home/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-#importing the model from models.py
-from home.models import Contact
-# Register your models here.
-admin.site.register(Contact)
-#Contact.objects.all()[0].name in shell for query
-#for filter=Contact.objects.filter(name="Vanshika Thapliyal",phone="07973379830")
